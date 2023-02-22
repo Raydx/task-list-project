85c2a0fa8d7a7e9900354d718cd355f902f20e9e
Le Plain François <francois.leplain@gmail.com>
2023-02-22 21:24:51 +0100

    ajout style

--- src/components/add-task/add-task.tsx
+++ src/components/add-task/add-task.tsx
@@ -34,11 +34,11 @@ export class AddTask {
     return (
       <form onSubmit={(event: Event) => this.handleSubmit(event)}>
         <div class="relative">
-          <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-lg focus:ring-blue-500 focus:border-blue-500 p-2"
-            value={this.value} onInput={(event) => this.handleChange(event)} placeholder="Ajouter une tâche" />
+          <input type="text" class="bg-gray-50 border font-style: italic border-black placeholder-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2"
+            value={this.value} onInput={(event) => this.handleChange(event)} placeholder="Ajouter une tâche.." />
 
-          <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-r-lg text-sm px-4 py-2">
-            Ajouter</button>
+          <button type="submit" class="focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-2 py-2">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="16" x="0" y="0" viewBox="0 0 448 448"><g><path d="M408 184H272a8 8 0 0 1-8-8V40c0-22.09-17.91-40-40-40s-40 17.91-40 40v136a8 8 0 0 1-8 8H40c-22.09 0-40 17.91-40 40s17.91 40 40 40h136a8 8 0 0 1 8 8v136c0 22.09 17.91 40 40 40s40-17.91 40-40V272a8 8 0 0 1 8-8h136c22.09 0 40-17.91 40-40s-17.91-40-40-40zm0 0" fill="#000000" data-original="#000000" class=""></path></g></svg></button>
         </div>
       </form>
     );
--- src/components/task-list/task-list.tsx
+++ src/components/task-list/task-list.tsx
@@ -27,11 +27,12 @@ export class TaskList {
   render() {
     return (
       <div>
-        <h2>Liste des tâches : </h2>
+        <h2 class="text-xl underline font-semibold">Liste des tâches : </h2>
+        <br />
         <ul>
           {this.tasks.map(task => (
             <li>
-              <span> - {task} &nbsp;</span>
+              <span class="text-base font-mono"> - {task} &nbsp;</span>
               <button type="submit" onClick={() => this.deleteTask(task)}><svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="15" height="15" x="0" y="0" viewBox="0 0 511.992 511.992"><g><path fill="#e76e54" d="m415.402 495.422-159.406-159.41L96.59 495.422c-22.098 22.094-57.922 22.094-80.02 0-22.093-22.098-22.093-57.922 0-80.02l159.41-159.406L16.57 96.59c-22.093-22.098-22.093-57.922 0-80.02 22.098-22.093 57.922-22.093 80.02 0l159.406 159.41L415.402 16.57c22.098-22.093 57.922-22.093 80.02 0 22.094 22.098 22.094 57.922 0 80.02l-159.41 159.406 159.41 159.406c22.094 22.098 22.094 57.922 0 80.02-22.098 22.094-57.922 22.094-80.02 0zm0 0" data-original="#e76e54" class=""></path></g></svg></button>
             </li>
           ))}
